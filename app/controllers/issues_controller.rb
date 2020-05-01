28cade0645ef3e080b85fea459d6e9f67bf92b0f
emma levi <emmaa-l@hotmail.fr>
2020-05-01 14:04:38 +0300

    added had many chairties in issue model

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,6 @@ class IssuesController < ApplicationController
 
   def index
     @issues = Issue.geocoded #returns issues with coordinates
-
     @markers = @issues.map do |issue|
       {
         lat: issue.latitude,
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,5 @@
 class Issue < ApplicationRecord
   geocoded_by :location
+  has_many: charities
   after_validation :geocode, if: :will_save_change_to_location?
 end

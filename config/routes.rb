cc679303fe31274f739ff7b57c8fccbbc9156381
emma levi <emmaa-l@hotmail.fr>
2020-05-01 13:14:20 +0300

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'issues#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#Issues
+  get    "issues", to: "issues#index"
+  get    "issues/:id", to: "issues#show", as: :issue
+
+#Charities
+  get    "charities", to: "charities#index"
+  get    "charities/:id", to: "charities#show", as: :charity
+
+#Donations
+get    "charities/:charity_id/donations/new", to: "donations#new",  as: :new_donation
+post   "charities/:charity_id/donations", to: "donations#create"
+get    "donations/:id", to: "donations#show", as: :donation
+
 end

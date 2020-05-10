eeb6ceddd61a6255d012910a5daf6333a87e0965
Alex Chernovsky <alexche8888@gmail.com>
2020-05-10 21:40:27 +0300

    Add charity show view page

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,9 +1,5 @@
 class CharitiesController < ApplicationController
 
-  def index
-    @charities = Issue.find(params[:issue_id]).charities
-  end
-
   def show
     @charity= Charity.find(params[:id])
   end

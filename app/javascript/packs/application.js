31d82032d66bd60f9a7b3b2f419ba33a87c8a07c
emma levi <emmaa-l@hotmail.fr>
2020-05-22 10:47:28 +0300

    done with the welcome page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,6 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-
-import { loadDynamicBannerText } from '../plugins/banner';
-loadDynamicBannerText();
-
 initMapbox();
 
 !(function($) {

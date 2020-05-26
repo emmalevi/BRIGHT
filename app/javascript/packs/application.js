8590b63489412dfa609c4fb6b8d9cbb5fe562852
Sagi Amar <sagiamar@Sagis-MacBook-Pro.local>
2020-05-26 21:46:44 +0300

    almost done with charity carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,13 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initSwiper } from '../plugins/init_charities_carousel';
 import { initDonateAmounts } from '../plugins/init_donate_amounts';
 
 initMapbox();
 
 document.addEventListener('DOMContentLoaded', () => {
+  initSwiper()
   initDonateAmounts()
 })
 
---
+++ app/javascript/plugins/init_charities_carousel.js
@@ -0,0 +1,9 @@
+import Swiper from 'swiper';
+
+const initSwiper = () => {
+	var mySwiper = new Swiper('.swiper-container', {
+		
+	});
+}
+
+export { initSwiper }
